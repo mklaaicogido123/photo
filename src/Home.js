338bb306a9d49302ab263252962c45a9b123de00
DuyPhong <mklaaicogido123@gmail.com>
2023-11-12 20:41:15 +0700

    change welcome page

--- src/Home.js
+++ src/Home.js
@@ -6,15 +6,9 @@ export default function Home() {
   return (
     <FadeIn>
       <div className="homeContainer">
-        <h1>Welcome to my gallery</h1>
+        <h1>Welcome</h1>
         <h2>This is where I kept all of my photo collection.</h2>
         <img src="/Images/camera.png" alt="camera" className="homePic" />
-        <a
-          href="https://pngtree.com/so/cartoon-clipart"
-          className="homePicDesc"
-        >
-          cartoon-clipart png from pngtree.com
-        </a>
       </div>
     </FadeIn>
   );
